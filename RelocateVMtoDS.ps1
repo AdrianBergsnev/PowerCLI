2eb7ddef3425d8707a3019e50a683f8ebdc03cbe
Adrian Bergsnev <adrian.bergsnev@gmail.com>
2017-06-01 12:38:07 +0200

    Added script

---
+++ RelocateVMtoDS.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module Vmware.vimautomation.core
+
+Connect-VIServer "Host"
+
+
+$VMname = "VM"
+
+$VMs = Get-VM "Cypher"
+
+$DS = Get-Datastore -Name "Datastore"
+
+$VMs | Move-VM -Datastore $DS
+
