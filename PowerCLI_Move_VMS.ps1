e2aed395d1a235ba4f9356b66f9e8224dead40f1
Adrian Bergsnev <adrian.bergsnev@gmail.com>
2017-06-01 12:39:48 +0200

    Added script

---
+++ PowerCLI_Move_VMS.ps1
@@ -0,0 +1,38 @@
+﻿Import-Module Vmware.vimautomation.core
+
+Connect-VIServer 192.168.0.9
+
+$List = @(
+"neo"
+"mouse"
+
+)
+$Collection = @()
+$List | foreach {
+    $VMName = $_
+
+    $Collection += Get-VM | where { $_.Name -like "*$VMName*" }
+}
+
+$Collection.Name
+
+<#
+
+$VMs = Get-VM | where { $PSItem.Name -like "16*" }
+
+$half = ($VMs.Count) / 2
+$half
+
+$i = 1
+$VMs | foreach {
+
+
+    if ($i -le $half)
+    {
+        $_ | Move-VM -Destination 192.168.0.21
+        
+    }
+
+    $i++
+}
+#>
